ea4999daaac8a3bfd59c67ba3ac5bb92138bb73c
Chris McKenzie <chris@oblong.com>
2011-08-25 19:16:42 -0700

    Support for WebBrick / Support for basic net.stream

--- nodestyle.rb
+++ nodestyle.rb
@@ -2,6 +2,6 @@ server = HTTP::server
 
 server.on('request') { |request, response| 
   response.writeHead 200, "Content-Type" => "text/html"
-  response.write "This is the node style calling convention. You can return <a href=/>to the root</a> to get the traditional ruby style."
-  response.doEnd
+  response.doEnd "This is the node style calling convention. You can return <a href=/>to the root</a> to get the traditional ruby style."
+  puts "Content After"
 }
